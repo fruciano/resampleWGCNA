72d4d8495d7d76473b6252c256ca9c4ccb5f8dee
Carmelo Fruciano <36262878+fruciano@users.noreply.github.com>
2019-03-13 15:32:01 +0100

    Initial commit

---
+++ R/test_RV_target_module.R
@@ -0,0 +1,59 @@
+#' Analysis of the association between a target module and one or more variables
+#'
+#'
+#' Given a dataset, a set of results from blockwiseModules, a target module, and one or more variables,
+#' this function performs a permutation test based on Escoufier RV.
+#' Optionally, it can also compute rarefied estimates of RV.
+#' Notice that this function requires the package GeometricMorphometricsMix
+#' and that the analysis can be computationally demanding for large modules
+#'
+#' @param OriginalData Matrix or data frame
+#' containing the original data on which the blockwiseModules function has been run
+#' (observations in rows, variables in columns).
+#' @param Original_blockwiseModules output of the blockwiseModules function
+#' @param target_module module whose association will be assessed
+#' @param X one or more variables whose association with the target module will be tested
+#' @param permutations number of permutations to use in the test of association
+#' @param rarefied if TRUE, it also performs rarefaction-based estimation of Escoufier RV
+#' @param reps_rarefaction number of replicated rarefied samples
+#' @param samplesize_rarefaction sample size to which rarefy (required if rarefied=TRUE)
+#'
+#' @return The function outputs a list with the following elements:
+#'  \describe{
+#'   \item{ObsRV}{Observed RV coefficient}
+#'   \item{p_value_perm}{p value obtained through permutation}
+#'   \item{Rarefied_RV}{Results of the rarefaction analysis (empty if rarefied=FALSE)}
+#' }
+#'
+#' @export
+test_RV_target_module=function(OriginalData, Original_blockwiseModules, target_module, X, permutations=999, rarefied=FALSE, reps_rarefaction=1000, samplesize_rarefaction=NULL) {
+
+	extract_data_expr_target_module=OriginalData[,which(Original_blockwiseModules$colors==target_module)]
+	# Extract only the original data corresponding to the selected target module
+
+		if (rarefied==TRUE) {
+			if (length(samplesize_rarefaction)==0) {
+			stop(paste("rarefied set to TRUE but no samplesize_rarefaction provided"))
+			}
+		Rarefied_RV=GeometricMorphometricsMix::RVrarefied(Block1=extract_data_expr_target_module,
+												Block2=X, reps = reps_rarefaction, samplesize=samplesize_rarefaction)
+		} else { Rarefied_RV=NA }
+	# Subroutine to compute rarefied estimates of Escoufier RV
+
+	ObsRV=GeometricMorphometricsMix::EscoufierRV(extract_data_expr_target_module, X)
+	# Observed value of RV
+
+	n_obs=nrow(X)
+	permuted_indices=lapply(seq_len(permutations), function(x) sample(seq_len(n_obs), n_obs, replace=FALSE))
+	permuted_X=lapply(permuted_indices, function(x) cbind(X[x,]) )
+	# Create permuted X
+
+	permuted_RV=unlist(lapply(permuted_X, function(x) GeometricMorphometricsMix::EscoufierRV(extract_data_expr_target_module, x)))
+	p_value_perm = (length(which(permuted_RV >= ObsRV))+1)/(permutations + 1)
+	# Compute permuted RV and corresponding p value
+
+	Results=list(Observed_RV=ObsRV,
+					p_value_perm=p_value_perm,
+					Rarefied_RV=Rarefied_RV)
+return(Results)
+	}
---
+++ R/test_eigengene_explained_variance.R
@@ -0,0 +1,88 @@
+#' Test the significance of a target eigengene
+#'
+#' Performs a permutation test of significance for the eigengene
+#'
+#' Given a dataset, a set of results from blockwiseModules, and a target module,
+#' this function performs a permutation test of the variance explained by the eigengene
+#' (first eigenvalue of the correlation matrix of the genes belonging to the module).
+#' The target module should be expressed in the same way as when using blockwiseModules
+#' (i.e., color or number)
+#' Notice that option bicor=TRUE can be extremely computationally demanding
+#'
+#' @param OriginalData Matrix or data frame
+#' containing the original data on which the blockwiseModules function has been run
+#' (observations in rows, variables in columns).
+#' @param Original_blockwiseModules output of the blockwiseModules function
+#' @param target_module module whose largest eigenvalue/eigengene will be tested
+#' @param permutations number of permutations to use
+#' @param bicor whether one should use the standard eigengene function in WGCNA (default), or bicor
+#' @param ... further parameters to be passed to either moduleEigengenes or bicor
+#'
+#' @return The function outputs a list with the following elements:
+#'  \describe{
+#'   \item{moduleEigengenes_ALL}{The eigengenes for all the modules (will be empty if bicor=TRUE)}
+#'   \item{explained_variance_target_module}{The observed explained variance for the target module}
+#'   \item{p_value}{The p value obtained through permutation}
+#' }
+#'
+#' @export
+test_eigengene_explained_variance=function(OriginalData, Original_blockwiseModules, target_module, permutations=999, bicor=FALSE, ...) {
+
+	extract_data_expr_target_module=OriginalData[,which(Original_blockwiseModules$colors==target_module)]
+	# Extract only the original data corresponding to the selected target module
+
+
+	if (bicor==FALSE) {
+	moduleEigengenes_ALL=moduleEigengenes(OriginalData,colors=Original_blockwiseModules$colors, ...)
+	target_eigengene_ME=which(colnames(moduleEigengenes_ALL$eigengenes)==paste("ME", target_module, sep=""))
+	observed_explained_variance=moduleEigengenes_ALL$varExplained[target_eigengene_ME]
+	# Compute module eigengenes for all the modules in the Original_blockwiseModules
+	# and select the explained variance for the target module
+	} else {
+	eigen_bicor_observed=eigen(bicor(extract_data_expr_target_module))$values
+	observed_explained_variance=(eigen_bicor_observed/sum(eigen_bicor_observed))[1]
+	moduleEigengenes_ALL=NA
+	# use bicor and eigenvalue decomposition to compute the first eigenvalue
+	}
+
+
+	permuted_indices=lapply(seq_len(permutations), function(X)
+							lapply(seq_len(ncol(extract_data_expr_target_module)), function(z)
+								sample(seq_len(nrow(extract_data_expr_target_module)),
+											nrow(extract_data_expr_target_module), replace=FALSE)
+							)	)
+	permuted_datasets=lapply(permuted_indices, function(x)
+		as.data.frame(matrix(NA, nrow(extract_data_expr_target_module), ncol(extract_data_expr_target_module)))
+		)
+		# preallocate list
+		for (k in seq_len(length(permuted_datasets))) {
+				for (j in seq_len(ncol(extract_data_expr_target_module))) {
+					permuted_datasets[[k]][,j]=extract_data_expr_target_module[permuted_indices[[k]][[j]],j]
+
+				}
+		}
+		# Generate permuted datasets with the genes in the target module
+
+	colors_permutations=rep(1, ncol(permuted_datasets[[1]]))
+	# simple vector of ones
+	if (bicor==FALSE) {
+	varExplained_permuted_datasets=unlist(lapply(permuted_datasets, function(X)
+				moduleEigengenes(X,colors=colors_permutations,...)$varExplained))
+				} else {
+	varExplained_permuted_datasets=unlist(lapply(permuted_datasets, function(X)
+					{ k=eigen(bicor(X))$values
+						(k/sum(k))[1]
+					} ))
+				}
+
+	names(varExplained_permuted_datasets)=paste("perm_", seq_len(permutations), sep="")
+	p_value_perm= (length(which(varExplained_permuted_datasets >= observed_explained_variance))+1)/(permutations + 1)
+	# Compute for each permuted dataset variance explained and a p value
+
+	Results=list(moduleEigengenes_ALL,
+					explained_variance_target_module=observed_explained_variance,
+					explained_variance_permuted_datasets=varExplained_permuted_datasets,
+					p_value=p_value_perm)
+	# create results list to return
+return(Results)
+}
